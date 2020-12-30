d9a963e0b6cf1e79d5ccc31f815d69d17cdf9494
Guilherme-Vasconcelos <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2020-12-30 17:17:33 -0300

    Add command to get last wallpaper

--- src/wallpaper-panic.sh
+++ src/wallpaper-panic.sh
@@ -5,8 +5,12 @@ CONFIG_DIR="$HOME/.config/wallpaper-panic"
 # Ensure wallpaper-panic directory exists
 mkdir -p $CONFIG_DIR
 
-# Create status file and adds "current_is_temp=1" if does not exist yet
+# Create status file and adds "current_is_tmp=1" if does not exist yet
 if [ ! -e "$CONFIG_DIR/status" ]
 then
-    echo "current_is_temp=1" > "$CONFIG_DIR/status"
+    echo "current_is_tmp=1" > "$CONFIG_DIR/status"
+fi
+
+if [ $DESKTOP_SESSION == "xfce" ]
+    # Get current wallpaper's path: xfconf-query -c xfce4-desktop -p /backdrop/screen0/$(xrandr|awk '/\<connected/{print "monitor"$1}')/workspace0/last-image
 fi
